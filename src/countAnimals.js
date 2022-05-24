e84ba0574d0e21afd8617209866f277f86ec26b6
Manuela França <manuelaalves26@gmail.com>
2022-05-24 16:53:09 -0300

    Criei a função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => ({ ...acc, [name]: residents.length }), {});
+  }
+  const { specie, sex } = animal;
+  if (sex) {
+    return species.find((specieName) => specieName.name === specie).residents
+      .filter(({ sex: sexo }) => sexo === sex).length;
+  }
+  return species.find((quantSpecies) => quantSpecies.name === specie).residents.length;
 }
+
+// { specie: 'giraffes', sex: 'female' }
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
