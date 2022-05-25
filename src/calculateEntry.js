618ddb262bb7b8c2146402311958950242a959a6
Manuela França <manuelaalves26@gmail.com>
2022-05-25 18:02:34 -0300

    Criei a função countEntrants.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui!
+  return entrants.reduce((acc, { age }) => {
+    if (age < 18) acc.child += 1;
+    if (age >= 18 && age < 50) acc.adult += 1;
+    if (age >= 50) acc.senior += 1;
+    return acc;
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = {}) {
+  if (Object.keys(entrants).length === 0) return 0;
+  const { adult: adulto, child: criança, senior: senhor } = countEntrants(entrants);
+  const { adult, senior, child } = prices;
+  return adulto * adult + criança * child + senhor * senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
